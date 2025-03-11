da988842a79872f28a7075fa7303330fac1f960b
OfficialGoodShit <officialgoodshit@gmail.com>
2025-03-11 15:21:48 +0800

    Update officialgoodshit.py

--- officialgoodshit.py
+++ officialgoodshit.py
@@ -1,6 +1,6 @@
 import requests
 
-__ENDPOINT_URL__: str = "https://tryme.squareweb.app/api"
+__ENDPOINT_URL__: str = "https://SupremeGod.squareweb.app/api"
 
 class OfficialGoodShit:
     def __init__(self, access_key) -> None:
