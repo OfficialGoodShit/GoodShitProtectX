9707b9f093bb876673c1e6cbbe70dd497c9e9119
OfficialGoodShit <officialgoodshit@gmail.com>
2025-04-27 09:09:11 +0800

    Update aiden.py

--- aiden.py
+++ aiden.py
@@ -1,6 +1,6 @@
 import requests
 
-__ENDPOINT_URL__: str = "https://supremev1234.squareweb.app/api"
+__ENDPOINT_URL__: str = "https://aidenpy.squareweb.app/api"
 
 class OfficialGoodShit:
     def __init__(self, access_key) -> None:
