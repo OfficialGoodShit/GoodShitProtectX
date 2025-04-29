44adbb9f66d2274b4e4cafc29085b8f846454d1e
OfficialGoodShit <officialgoodshit@gmail.com>
2025-04-30 00:38:51 +0800

    Update BrixTool.py

--- BrixTool.py
+++ BrixTool.py
@@ -243,11 +243,11 @@ class OfficialGoodShit:
         return response_decoded.get("ok")  
         
      def rear_bumper(self, car_id):
-        payload = {
+    payload = {
         "account_auth": self.auth_token,
         "car_id": car_id,
-        }
-        params = {"key": self.access_key}
-        response = requests.post(f"{__ENDPOINT_URL__}/rear_bumper", params=params, data=payload)
-        response_decoded = response.json()
-        return response_decoded.get("ok")      
+    }
+    params = {"key": self.access_key}
+    response = requests.post(f"{__ENDPOINT_URL__}/rear_bumper", params=params, data=payload)
+    response_decoded = response.json()
+    return response_decoded.get("ok")
