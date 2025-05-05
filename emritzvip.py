450913af7664360dc85b918caab040686aced3e9
OfficialGoodShit <officialgoodshit@gmail.com>
2025-05-06 01:10:10 +0800

    Update emritzvip.py

--- emritzvip.py
+++ emritzvip.py
@@ -4,7 +4,7 @@ import requests
 import subprocess
 import urllib.parse
 
-__ENDPOINT_URL__: str = "https://brix-tool-premium.store/api"
+__ENDPOINT_URL__: str = "https://emritzvipapi.squareweb.app/api"
 
 class OfficialGoodShit:
     def __init__(self, access_key) -> None:
