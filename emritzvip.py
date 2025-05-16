aeeb38791d62b516d6ea345061f37a4f7df54f40
OfficialGoodShit <officialgoodshit@gmail.com>
2025-05-16 19:07:14 +0800

    Update emritzvip.py

--- emritzvip.py
+++ emritzvip.py
@@ -4,7 +4,7 @@ import requests
 import subprocess
 import urllib.parse
 
-__ENDPOINT_URL__: str = "https://emritzvipapiii.squareweb.app/api"
+__ENDPOINT_URL__: str = "https://popstool.io/api"
 
 class OfficialGoodShit:
     def __init__(self, access_key) -> None:
