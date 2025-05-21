a4214f1b6f8f6ef34a727e8b873f76625a1971fb
OfficialGoodShit <officialgoodshit@gmail.com>
2025-05-21 20:34:09 +0800

    Update EmritzVipV6.py

--- EmritzVipV6.py
+++ EmritzVipV6.py
@@ -12,7 +12,7 @@ from rich.style import Style
 import pystyle
 from pystyle import Colors, Colorate
 
-from testdevice import OfficialGoodShit
+from tesdevice import OfficialGoodShit
 
 def signal_handler(sig, frame):
     print("\n Bye Bye...")
